2988fd8b3c212c20a6527ae318c95948fd979234
Shake <kidustv.dev@gmail.com>
2023-07-23 23:20:04 +0200

    code optimising

--- Extensions/Commands/Other/language/language.py
+++ Extensions/Commands/Other/language/language.py
@@ -47,6 +47,8 @@ class command(ShakeCommand):
             print(locales)
         assert current is not None
 
+        # print(1)
+
         menu = ListMenu(
             ctx=self.ctx,
             source=PageSource(
